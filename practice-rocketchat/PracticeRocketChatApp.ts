f13b20111fac2ba4c97d396206dbeda08a132c3e
CulturalProfessor <svinayak994@gmail.com>
2023-03-10 00:45:30 +0530

    created hello world slash command

--- practice-rocketchat/PracticeRocketChatApp.ts
+++ practice-rocketchat/PracticeRocketChatApp.ts
@@ -1,12 +1,23 @@
 import {
     IAppAccessors,
+    IConfigurationExtend,
     ILogger,
-} from '@rocket.chat/apps-engine/definition/accessors';
-import { App } from '@rocket.chat/apps-engine/definition/App';
-import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+} from "@rocket.chat/apps-engine/definition/accessors";
+import { App } from "@rocket.chat/apps-engine/definition/App";
+import { IAppInfo } from "@rocket.chat/apps-engine/definition/metadata";
+import {HelloWorldCommand} from './Commands/HelloWorldCommand'
+
 
 export class PracticeRocketChatApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
+    }
+    public async extendConfiguration(
+        configuration: IConfigurationExtend
+    ): Promise<void> {
+        const helloWorldCommand: HelloWorldCommand = new HelloWorldCommand();
+        await configuration.slashCommands.provideSlashCommand(
+            helloWorldCommand
+        );
     }
 }
